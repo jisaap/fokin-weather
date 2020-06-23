888649ca10fab9677aa453a294d36aa79c8925e0
jisaap <odo919@naver.com>
2020-06-23 23:34:01 +0900

    2020-06-23

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={style.text}>Hello!!!</Text>
     </View>
   );
 }
@@ -12,8 +12,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
+    // 텍스트가 부모의 color를 상속 받지 않음
+    // color: "white",
     justifyContent: 'center',
   },
+  text:{
+    color:"white",
+    // width:"100px"   에러남
+  }
 });
